2b7d36d4fe3756a3f7362e14e686a580b9ab942d
Choppa Anil <choppaanil.kumar@infosys.com>
2025-11-06 08:35:19 +0000

    Session timeout

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "testapp/freestyle/model/models"
-], (UIComponent, models) => {
+    "testapp/freestyle/model/models",
+    "sap/m/MessageBox"
+], (UIComponent, models,MessageBox) => {
     "use strict";
 
     return UIComponent.extend("testapp.freestyle.Component", {
@@ -18,9 +19,31 @@ sap.ui.define([
 
             // set the device model
             this.setModel(models.createDeviceModel(), "device");
-
+             if (!sap.ushell || !sap.ushell.Container) {
+                this.handleAppTimeout();
+            }
             // enable routing
             this.getRouter().initialize();
-        }
+        },
+        handleAppTimeout: function () {
+            var timeoutmins, timer;
+            timeoutmins = 1;
+            function resetTimer() {
+                clearTimeout(timer);
+                timer = setTimeout(onTimeOut, timeoutmins * 60 *1000);
+            }
+            function onTimeOut() {
+                console.log('LOGOUT')
+                MessageBox.warning("your session is expired do to inactivity you will be logged out", {
+                    onClose: function () {
+                        window.location.reload();
+                    }
+                })
+            }
+            resetTimer();
+            window.onmousemove = resetTimer;
+            window.onkeypress = resetTimer;
+        },
+
     });
 });
